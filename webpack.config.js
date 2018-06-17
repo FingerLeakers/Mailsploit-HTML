0f82f4e594fc180a6fc5722daa0b70f22c64e6cb
pwnsdx <sabri@wire.com>
2018-06-17 11:33:26 +0200

    Cleanup in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -19,17 +19,14 @@ const appHtmlTitle = 'Mailsploit';
 const markdown = require('markdown-it')({html: true});
 
 const iterator = require('markdown-it-for-inline');
-/*markdown.use(iterator, 'add_span', 'text', (tokens, idx) => {
-    const token = tokens[idx];
-    // replace original
-    //tokens[idx].content = `<span>${token.content.split(' ').join('</span> <span>')}</span>`;
-});*/
+
 markdown.use(require('./app/markdown-it-video/index', { // <-- this use(package_name) is required
   youtube: { width: 480, height: 251, theme: 'white' },
   vimeo: { width: 500, height: 281 },
   vine: { width: 600, height: 600, embed: 'simple' },
   prezi: { width: 550, height: 400 }
 }));
+
 markdown.use(iterator, 'url_new_win', 'link_open', (tokens, idx) => {
     const currentLink = tokens[idx];
     currentLink.attrPush([ 'target', '_blank' ]);
@@ -41,67 +38,14 @@ markdown.use(iterator, 'url_new_win', 'link_open', (tokens, idx) => {
 
     currentLink.attrPush([ 'rel', rel ]);
 });
+
 markdown.renderer.rules['paragraph_open'] = (tokens, idx, options, env, self) => {
   return '<p><span>';
 };
+
 markdown.renderer.rules['paragraph_close'] = (tokens, idx, options, env, self) => {
   return '</span></p>';
 };
-
-/*markdown.renderer.rules.text = (tokens, idx) => {
-  // disable escaping, god dammit fuck that crap
-  return tokens[idx].content;
-};*/
-
-
-/*markdown.use(require('markdown-it-container'), 'paragraph', {
-  validate: (params) => params.trim() === 'paragraph',
-  //const m = tokens[idx].info.trim();
-  render: (tokens, idx) => {
-    console.log(tokens);
-    return (tokens[idx].nesting === 1 ? '<div>' : '</div>');
-  }
-});
-markdown.renderer.rules['paragraph_open'] = (tokens, idx, options, env, self) => {
-  return '<p class="test">';
-};*/
-
-/*markdown.renderer.rules['text'] = (tokens, idx, options, env, self) => {
-    console.log('INLINE');
-    //console.log(idx);
-    //console.log(tokens[idx]);
-    //console.log(tokens[idx - 1] === 'paragraph_open');
-
-    console.log(options);
-    console.log(env);
-    console.log(self);
-
-  return 'lOL';
-};*/
-
-
-/*markdown.core.ruler.at('replacements', function replace(state) {
-    //...
-    const tokens = state.tokens;
-    let interator = 0;
-    for (token of tokens) {
-        if (token.type === 'inline') {
-            if (tokens[interator - 1].type === 'paragraph_open') {
-                console.log('PARAGRAPHGE OMGGG');
-                // replace original
-                state.tokens[interator].content = 'tgtg';
-            }
-            console.log('inline maggle');
-        }
-        interator++;
-    }
-});
-*/
-
-
-/*markdown.renderer.rules['paragraph_close'] = (tokens, idx, options, env, self) => {
-  return '</p>';
-};*/
 
 /**
  * Webpack Configuration
@@ -111,9 +55,6 @@ module.exports = {
         crossOriginLoading: 'anonymous',
     },
     entry: {
-        /*vendor: [
-            'lodash'
-        ],*/
         bundle: path.join(dirApp, 'index')
     },
     resolve: {
@@ -127,11 +68,6 @@ module.exports = {
         new ExtractTextPlugin('[name].[chunkhash].css'),
 
         new FaviconsWebpackPlugin(path.join(dirAssets, 'images', 'favicon.png')),
-
-        /*new webpack.ProvidePlugin({
-            // lodash
-            '_': 'lodash'
-        }),*/
 
         new SriPlugin({
             hashFuncNames: ['sha256', 'sha384'],
